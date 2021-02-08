bb7a34777dd4cc8642e56e7a13e88848bb49fb21
Grégouzz <gregoirebourdin05@gmail.com>
2021-02-08 11:34:11 +0100

    Installation of tailwind and Webpack Encore

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    // Display the homepage.
+    #[Route('/home', name: 'homepage')]
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
