88a42bb514df1c04907d6be0686acc1a3893d268
Grégouzz <gregoirebourdin05@gmail.com>
2021-02-12 11:17:34 +0100

    Installation of dependencies

--- assets/app.js
+++ assets/app.js
@@ -12,5 +12,40 @@ import './styles/app.css';
 // import Alpinejs
 import 'alpinejs';
 
-// start the Stimulus application
+// import Scroll Reveal
+import ScrollReveal from 'scrollreveal';
+
+    // Animation of headline, tagline and punchline
+    ScrollReveal().reveal('.headline');
+    ScrollReveal().reveal('.tagline', { delay: 500 });
+    ScrollReveal().reveal('.punchline', { delay: 2000 });
+
+// import Plyr
+import Plyr from 'plyr';
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+        const player = new Plyr('#player');
+
+        // Expose
+        window.player = player;
+
+        // Bind event listener
+        function on(selector, type, callback) {
+            document.querySelector(selector).addEventListener(type, callback, false);
+        }
+
+        // Pause
+        on('.js-pause', 'click', () => {
+            player.pause();
+        });
+
+        // Play
+        on('.js-play', 'click', () => {
+            player.play();
+        });
+
+    });
+
+// Start the Stimulus application
 import './bootstrap';
