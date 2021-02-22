1563309bb26beabc23593ae0adf427fe4c3b52b9
Guillaume <hello@eloken.com>
2021-02-22 17:41:49 +0100

    Swup cleared

--- assets/app.js
+++ assets/app.js
@@ -11,52 +11,52 @@ import './styles/app.css';
 // import Alpinejs
 import 'alpinejs';
 
+
+// import Plyr
+import Plyr from 'plyr';
+
 // import Scroll Reveal
 import ScrollReveal from 'scrollreveal';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+    const player = new Plyr('#player');
+    console.log('DOM fully loaded and parsed');
+
+    // Expose
+    window.player = player;
+
+    // Bind event listener
+    function on(selector, type, callback) {
+        document.querySelector(selector).addEventListener(type, callback, false);
+        console.log('1');
+    }
+
+    // Pause
+    on('.js-pause', 'click', () => {
+        player.pause();
+        console.log('2');
+    });
+
+    // Pause 2
+    on('.js-pause2', 'click', () => {
+        player.pause();
+        console.log('3');
+    });
+
+    // Play
+    on('.js-play', 'click', () => {
+        player.play();
+        console.log('4');
+    });
 
     // Animation of headline, tagline and punchline
     ScrollReveal().reveal('.headline');
     ScrollReveal().reveal('.tagline', { delay: 500 });
     ScrollReveal().reveal('.punchline', { delay: 2000 });
     ScrollReveal().reveal('.highline', { delay: 1000 });
-
-// import Plyr
-import Plyr from 'plyr';
-
-    document.addEventListener('DOMContentLoaded', () => {
-        // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-        const player = new Plyr('#player');
-
-        // Expose
-        window.player = player;
-
-        // Bind event listener
-        function on(selector, type, callback) {
-            document.querySelector(selector).addEventListener(type, callback, false);
-        }
-
-        // Pause
-        on('.js-pause', 'click', () => {
-            player.pause();
-        });
-
-        // Pause 2
-        on('.js-pause2', 'click', () => {
-            player.pause();
-        });
-
-        // Play
-        on('.js-play', 'click', () => {
-            player.play();
-        });
-
-    });
-
-// Import Flickity
-const Flickity = require('flickity');
-const flkty = new Flickity('.carousel', {
-    // options...
 });
+
 
 // Start the Stimulus application
 import './bootstrap';
--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -1,16 +1,58 @@
 import { Controller } from 'stimulus';
+import Plyr from "plyr";
+import ScrollReveal from "scrollreveal";
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
 export default class extends Controller {
     connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+        this.element.addEventListener('swup:connect', this._onConnect);
     }
+
+    disconnect() {
+        // You should always remove listeners when the controller is disconnected to avoid side-effects
+        this.element.removeEventListener('swup:connect', this._onConnect);
+    }
+
+    _onConnect(event) {
+        document.addEventListener('swup:contentReplaced', (event) => {
+            // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+            const player = new Plyr('#player');
+            console.log('DOM fully loaded and parsed');
+
+            // Expose
+            window.player = player;
+
+            // Bind event listener
+            function on(selector, type, callback) {
+                document.querySelector(selector).addEventListener(type, callback, false);
+                console.log('1');
+            }
+
+            // Pause
+            on('.js-pause', 'click', () => {
+                player.pause();
+                console.log('2');
+            });
+
+            // Pause 2
+            on('.js-pause2', 'click', () => {
+                player.pause();
+                console.log('3');
+            });
+
+            // Play
+            on('.js-play', 'click', () => {
+                player.play();
+                console.log('4');
+            });
+
+            // Animation of headline, tagline and punchline
+            ScrollReveal().reveal('.headline');
+            ScrollReveal().reveal('.tagline', { delay: 500 });
+            ScrollReveal().reveal('.punchline', { delay: 2000 });
+            ScrollReveal().reveal('.highline', { delay: 1000 });
+        });
+
+    }
+
+
 }
