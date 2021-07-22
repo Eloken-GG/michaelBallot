169aa7cb4d0a488b8b2e39d8042a6b3eae9e188c
Guillaume <hello@eloken.com>
2021-07-22 17:17:58 +0200

    New seminaire and JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,47 @@
 module.exports = {
 
 
-  purge: [
-      "./templates/**/*.html.twig",
-      "./templates/*.html.twig"
+    mode: 'jit',
+    darkMode: false,
+    purge: {
+        content: [
+            "./templates/**/*.html.twig",
+            "./templates/*.html.twig"
+        ],
+        safelist: []
+    },
+
+    theme: {
+        extend: {
+
+            fontFamily: {
+                'montserrat': ['Montserrat', 'sans-serif'],
+                'playfair': ['Playfair Display', 'serif'],
+                'raleway': ['Raleway', 'sans-serif'],
+            },
+
+            colors: {
+                'rose': {
+                    DEFAULT: '#E30067',
+                    '50': '#FFC9E2',
+                    '100': '#FFB0D4',
+                    '200': '#FF7DB8',
+                    '300': '#FF4A9C',
+                    '400': '#FF1780',
+                    '500': '#E30067',
+                    '600': '#B00050',
+                    '700': '#7D0039',
+                    '800': '#4A0022',
+                    '900': '#17000A',
+                },
+            },
+        },
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
     ],
 
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
 
-      fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
-        'playfair': ['Playfair Display', 'serif'],
-        'raleway': ['Raleway', 'sans-serif'],
-      },
-
-      colors: {
-        'rose': {
-          DEFAULT: '#E30067',
-          '50': '#FFC9E2',
-          '100': '#FFB0D4',
-          '200': '#FF7DB8',
-          '300': '#FF4A9C',
-          '400': '#FF1780',
-          '500': '#E30067',
-          '600': '#B00050',
-          '700': '#7D0039',
-          '800': '#4A0022',
-          '900': '#17000A',
-        },
-        },
-      },
-    },
-
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-
-  variants: {
-    extend: {
-      appearance: ['hover', 'focus'],
-    },
-  },
-
-  corePlugins: {
-    outline: false,
-  },
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,10 @@ const glob = require('glob-all');
 const path = require('path')
 
 
-
-
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'production');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
@@ -86,29 +84,6 @@ Encore
 // uncomment if you're having problems with a jQuery plugin
 //.autoProvidejQuery()
 
-
-
-
-
-
-if (Encore.isProduction()) {
-    Encore.addPlugin(new PurgeCssPlugin({
-
-        safelist: [ "aos-init", "aos-animate", "data-aos-delay", "data-aos-duration", "fade-up", "fade-left", "fade-right", "fade-left", "/plyr$/" ],
-
-
-        paths: glob.sync([
-            path.join(__dirname, 'templates/**/*.html.twig'),
-            path.join(__dirname, 'templates/*.html.twig')
-        ]),
-        defaultExtractor: (content) => {
-            return content.match(/[\w-/:]+(?<!:)/g) || [];
-
-        },
-    }));
-
-}
-;
 
 
 
